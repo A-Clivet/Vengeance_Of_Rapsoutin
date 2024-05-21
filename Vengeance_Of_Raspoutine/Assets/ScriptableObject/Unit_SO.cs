bd7fcfd0876b9653ede76cf45b56e15026238c6b
MDKayzeur <mdricmaurice@gmail.com>
2024-05-21 10:06:36 +0200

    Unit Scriptable Object Creation

---
+++ Vengeance_Of_Raspoutine/Assets/ScriptableObject/Unit_SO.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Unity_Data", menuName = "ScriptableObjects/UnityData", order = 1)]
+public class SpawnManagerScriptableObject : ScriptableObject
+{
+    public string UnitName;
+    /* unit stats */
+    public int attack;
+    public int defense;
+
+    /*grid variable*/
+    public int sizeX;
+    public int sizeY;
+
+    /*unit display and match variable*/
+    public Image unitSprite;
+    public int unitType;
+    public int unitColor;
+}
