8260dd838eb8ef06c799ef234cfee84346caa3ec
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-05-22 17:25:34 +0200

    Grid Unit
    Bug fixed where unit became unteractable

---
+++ Vengeance_Of_Raspoutine/Assets/Scripts/Unit/S_RemoveUnit.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static S_Instantiator;
+
+public class S_RemoveUnit : MonoBehaviour
+{
+    public static S_RemoveUnit Instance;
+
+    public Unit hoveringUnit;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    public void DestroyUnit(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (hoveringUnit != null)
+            {
+                hoveringUnit._grid.unitList.Remove(hoveringUnit);
+                hoveringUnit.actualTile.unit = null;
+                foreach (S_Tile tile in hoveringUnit._grid.gridList[hoveringUnit.tileX])
+                {
+                    if (tile.unit != null)
+                    {
+                        tile.unit.MoveToTile(hoveringUnit.actualTile);
+                    }
+                }
+                Destroy(hoveringUnit.gameObject);
+                hoveringUnit = null;
+            }
+        }
+    }
+}
