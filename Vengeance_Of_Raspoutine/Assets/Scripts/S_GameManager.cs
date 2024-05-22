f813bf01e335c079c02cc8ec0f66bcefb604cc12
lukabartlo <l.bartolo@lahorde.school>
2024-05-22 16:31:35 +0200

    GameManager :  Ajout d'un affichage de Tour , Ajout de l'écran de victoire de chaque joueur,  Ajout d'un Instance System
    -Possibilité d'avoir la reférence du game manager
    -Possibilité de voir quel joueur joue actuellement
    -Possibilité de voir de nombre de Tours
    -L'image de victoire du joueur 1 ou 2 est afficher selon le joueur qui gagne

--- Vengeance_Of_Raspoutine/Assets/Scripts/S_GameManager.cs
+++ Vengeance_Of_Raspoutine/Assets/Scripts/S_GameManager.cs
@@ -5,33 +5,50 @@ using UnityEngine.UI;
 
 public class S_GameManager : MonoBehaviour
 {
+    public static S_GameManager Instance;
+
     [Header("Background references :")]
     public List<Sprite> mapSelection = new(new Sprite[5]);
     [SerializeField] private Image _currentSprite; // sprite that display the map 
 
     [Header("Turn references :")]
     [SerializeField] private TextMeshProUGUI _timerText;
+    [SerializeField] private TextMeshProUGUI _playerTurnText;
+    [SerializeField] private TextMeshProUGUI _turnsText;
     
     [Header("Character's stats references :")]
     [SerializeField] S_CharacterStats _character1Stats;
     [SerializeField] S_CharacterStats _character2Stats;
 
+    [Header("Player 1 and player 2 end screen :")]
+    [SerializeField] private GameObject _player1EndScreen;
+    [SerializeField] private GameObject _player2EndScreen;
+
     private float _targetTime;
+    private int _currentTurnNumber;
     private bool _isPlayer1Turn = true;
     
     private int _player1Point;
     private int _player2Point;
     private Sprite _currentMap;
     
-    private int _intMap = 2;// int for the current time in the list
+    private int _intMap = 2;    // int for the current time in the list
 
+    private void Awake()
+    {
+        Instance = S_Instantiator.Instance.ReturnInstance(this, Instance, S_Instantiator.InstanceConflictResolutions.DestructionOfTheSecondOne);
+    }
 
     private void Start()
     {
         _targetTime = 3f;
+        _currentTurnNumber = 1;
         _timerText.text = _targetTime.ToString();
         _currentMap = mapSelection[_intMap];  // set the current map on start
         _currentSprite.sprite = mapSelection[_intMap]; // set the current sprite on start
+
+        _player1EndScreen.SetActive(false);
+        _player2EndScreen.SetActive(false);
 
         S_CharacterManager.Instance.SpawnCharacter(_character1Stats, true);
         S_CharacterManager.Instance.SpawnCharacter(_character2Stats, false);
@@ -41,16 +58,28 @@ public class S_GameManager : MonoBehaviour
     private void Update()
     {
         _targetTime -= Time.deltaTime; //decrease the timer 
-        _timerText.text = ((int)_targetTime).ToString(); // display the timer in a text and rounded off in seconds
+        _timerText.text = "Remaining Time : " + ((int)_targetTime).ToString(); // display the timer in a text and rounded off in seconds
+        _turnsText.text = "Turns : " + _currentTurnNumber.ToString(); //display the current round number
 
         if (_targetTime <= 0.0f) //check if the timer is equal or less to 0
         {
             TimerEnded();
         }
+
+        if(_isPlayer1Turn) 
+        {
+            _playerTurnText.text = "Player 1 Turn";
+        }
+
+        else if (!_isPlayer1Turn)
+        { 
+            _playerTurnText.text = "Player 2 Turn";
+        }
     }
 
 
-    /// <summary> Change the map when the player lose a point or win a point and add a point to the player 1 or 2 </summary>
+    /// <summary> Change the map when the player lose a point or win a point and add a point to the player 1 or 2
+    ///           and checks if the player 1 or 2 wins </summary>
     /// <param name="p_isPlayer1Dead"></param>
     public void HandlePlayerLose(bool p_isPlayer1Dead)
     {
@@ -65,13 +94,23 @@ public class S_GameManager : MonoBehaviour
             _intMap += 1;
         }
 
+        if (_player1Point >= 3)
+        {
+            _player1EndScreen.SetActive(true);
+        }
+
+        if (_player2Point >= 3)
+        {
+            _player2EndScreen.SetActive(true);
+        }
+
         _currentMap = mapSelection[_intMap];
         _currentSprite.sprite = mapSelection[_intMap];
 
         // TODO : Show a UI of defeat to give feedback to the player
     }
 
-    private void TimerEnded() // change the turn of the player and reset the timer to 60s
+    private void TimerEnded() // change the turn of the player and reset the timer to 60s and adds 1 to the current round number
     {
         if (_isPlayer1Turn)
         {
@@ -80,8 +119,8 @@ public class S_GameManager : MonoBehaviour
         else if (!_isPlayer1Turn)
         {
             _isPlayer1Turn = true;
+            _currentTurnNumber += 1;
         }
-
-        _targetTime = 60.0f;
+        _targetTime = 15.0f;
     }
 }
