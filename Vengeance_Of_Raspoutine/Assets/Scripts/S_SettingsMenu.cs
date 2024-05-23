17430b6e78cfc36a90812d0d5267ea011b081017
xinom4966 <sd.dessay@gmail.com>
2024-05-23 17:14:21 +0200

    settings menu : save settings(b)
    - the settings are now saved and will reapply after restarting the game.

--- Vengeance_Of_Raspoutine/Assets/Scripts/S_SettingsMenu.cs
+++ Vengeance_Of_Raspoutine/Assets/Scripts/S_SettingsMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class S_SettingsMenu : MonoBehaviour
 {
     [SerializeField] private TMP_Dropdown _screenSizeDropdown;
+    [SerializeField] private Toggle _fullScreenToggle;
     private Resolution[] _resolutions;
     private List<Resolution> _filteredResolutions;
     private float _currentRefreshRate;
@@ -16,11 +18,11 @@ public class S_SettingsMenu : MonoBehaviour
         _resolutions = Screen.resolutions;
         _filteredResolutions = new List<Resolution>();
         _screenSizeDropdown.ClearOptions();
-        _currentRefreshRate = Screen.currentResolution.refreshRate;
+        _currentRefreshRate = Screen.currentResolution.refreshRateRatio.numerator;
 
         for (int i=0; i < _resolutions.Length; i++)
         {
-            if (_resolutions[i].refreshRate == _currentRefreshRate)
+            if (_resolutions[i].refreshRateRatio.numerator == _currentRefreshRate)
             {
                 _filteredResolutions.Add(_resolutions[i]);
             }
@@ -30,7 +32,7 @@ public class S_SettingsMenu : MonoBehaviour
 
         for (int i=0; i < _filteredResolutions.Count; i++)
         {
-            string resolutionOption = _filteredResolutions[i].width + "x" + _filteredResolutions[i].height + " " + _filteredResolutions[i].refreshRate + "Hz";
+            string resolutionOption = _filteredResolutions[i].width + "x" + _filteredResolutions[i].height + " " + _filteredResolutions[i].refreshRateRatio.numerator + "Hz";
             options.Add(resolutionOption);
             if (_filteredResolutions[i].width==Screen.width && _filteredResolutions[i].height == Screen.height)
             {
@@ -41,18 +43,52 @@ public class S_SettingsMenu : MonoBehaviour
         _screenSizeDropdown.AddOptions(options);
         _screenSizeDropdown.value = _currentResolutionIndex;
         _screenSizeDropdown.RefreshShownValue();
+
+        LoadSavedDatas();
     }
 
     public void SetResolution()
     {
         Resolution resolution = _filteredResolutions[_currentResolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, true);
+        PlayerPrefs.SetInt("Resolution", _currentResolutionIndex);
     }
 
     public void SetFullScreen(bool value)
     {
         _isFullScreen = value;
         Screen.fullScreen = value;
+        if (_isFullScreen )
+        {
+            PlayerPrefs.SetInt("isFullScreen", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("isFullScreen", 0);
+        }
+    }
+
+    private void LoadSavedDatas()
+    {
+        if (PlayerPrefs.GetInt("isFullScreen") == 0)
+        {
+            _isFullScreen = false;
+            Screen.fullScreen = false;
+            _fullScreenToggle.isOn = false;
+        }
+        else
+        {
+            _isFullScreen = true;
+            Screen.fullScreen = true;
+            _fullScreenToggle.isOn = false;
+        }
+
+        _currentResolutionIndex = PlayerPrefs.GetInt("resolution");
+
+        _screenSizeDropdown.value = _currentResolutionIndex;
+        _screenSizeDropdown.RefreshShownValue();
+
+        SetResolution();
     }
 
 }
