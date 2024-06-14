188fcf962fd320dfe6e3f59813899957a3443daf
NicolasTre <N.TREZEGUET@lahorde.school>
2024-06-14 16:09:39 +0200

    Menu Selection Adversaire
    Ajout d'ui pour le selection d'adversaire entre l'ia ou un joueur + création du script de base

---
+++ Vengeance_Of_Raspoutine/Assets/Scripts/Menu/S_SelectionMode.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_SelectionMode : MonoBehaviour
+{
+
+    public void OnIAButton()
+    {
+        // function pour activer le mode IA contre joueur
+    }
+
+    public void OnPlayerButton()
+    {
+        // function pour activer le mode player versus player
+    }
+}
