d5f5d99581230527dfdb7f160d8fbae274ea7f1a
NicolasTre <N.TREZEGUET@lahorde.school>
2024-06-18 11:10:19 +0200

    proprety of code
    proprety of code for the script Upgrade Handler

--- Vengeance_Of_Raspoutine/Assets/Scripts/S_UpgradeHandler.cs
+++ Vengeance_Of_Raspoutine/Assets/Scripts/S_UpgradeHandler.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class S_UpgradeHandler : MonoBehaviour
@@ -8,17 +9,10 @@ public class S_UpgradeHandler : MonoBehaviour
     public GameObject _player1UpgradeCanvas;
     public GameObject _player2UpgradeCanvas;
     [SerializeField] private GameObject _animPlayerCanvas;
-    private bool _isFinish = false; 
 
     private void Awake()
     {
         Instance = S_Instantiator.Instance.ReturnInstance(this, Instance, S_Instantiator.InstanceConflictResolutions.WarningAndPause);
-    }
-
-    private void Start()
-    {
-        S_WeatherAnimation.Instance.hasPlayedPlayer = false;
-        S_WeatherAnimation.Instance.hasPlayedWeather = false;
     }
 
     public void NextBtn()
@@ -27,26 +21,22 @@ public class S_UpgradeHandler : MonoBehaviour
         _player2UpgradeCanvas.SetActive(true);
     }
 
-    public void Finished()
+    public void Finished() //Button that manages the last troop upgrade canvas to start a new game
     {
         _player2UpgradeCanvas.SetActive(false);
-        
         S_GameManager.Instance.HandlePlayerLose(S_GameManager.Instance.isLastPlayerDeadIsPlayer1);
         _animPlayerCanvas.SetActive(false);
-
         S_GameManager.Instance.isGameRunning = true;
         StartCoroutine(AnimSwitch());
     }
 
-    public IEnumerator AnimSwitch()
+    public IEnumerator AnimSwitch() // Coroutine to manage weather animation and switch to player animation after the first round 
     {
-        
         S_WeatherAnimation.Instance.PlayWeatherAnimation();
         Time.timeScale = 1.0f;
         yield return new WaitForSeconds(2.5f);
         _animPlayerCanvas.SetActive(true);
         S_WeatherAnimation.Instance.PlayAnimAfterTheOther();
         yield return new WaitForSeconds(3f);
-        
     }
 }
