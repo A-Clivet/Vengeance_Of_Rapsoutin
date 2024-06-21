ed60ddede66b635894467ba39685962b4d12a39f
NicolasTre <N.TREZEGUET@lahorde.school>
2024-06-21 10:04:35 +0200

    fix bug Select unit menu and delete warning  for a  animation

--- Vengeance_Of_Raspoutine/Assets/Scripts/Menu/S_Credits.cs
+++ Vengeance_Of_Raspoutine/Assets/Scripts/Menu/S_Credits.cs
@@ -5,22 +5,25 @@ using UnityEngine.Serialization;
 public class S_Credits : MonoBehaviour
 {
     [SerializeField] private Animator animatorCredits;
+    [SerializeField] private GameObject _canvasCredits;
     public string animationName;
 
     // Update is called once per frame
     void Update()
     {
-
-        AnimatorStateInfo stateInfo = animatorCredits.GetCurrentAnimatorStateInfo(0);
-
-        if (stateInfo.IsName(animationName))
+        if (_canvasCredits.activeSelf)
         {
-            // check if the animation is finish
-            if (stateInfo.normalizedTime >= 1f)
+            AnimatorStateInfo stateInfo = animatorCredits.GetCurrentAnimatorStateInfo(0);
+
+            if (stateInfo.IsName(animationName))
             {
-                // when the animation credits is finish, return automaticaly in the main menu
-                SceneManager.LoadScene("MainMenu");
+                // check if the animation is finish
+                if (stateInfo.normalizedTime >= 1f)
+                {
+                    // when the animation credits is finish, return automaticaly in the main menu
+                    SceneManager.LoadScene("MainMenu");
+                }
             }
-        }
+        }  
     }
 }
